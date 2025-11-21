966752b84ce8410044b345593d508476dfedca79
clickalot <rstavig@comcast.net>
2025-11-21 12:23:45 -0600

    rmv unneeded seed file

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,25 +1,17 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { neonConfig } from '@neondatabase/serverless';
+
+
+import dotenv from 'dotenv'
 import ws from 'ws';
 
-// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
+dotenv.config()
+
+
 neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool({ connectionString });
 
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool);
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-export const prisma = new PrismaClient({ adapter }).$extends({
-  result: {
-    todo: {
-      
-      },
-      
-    },
-  },
-);
+export const prisma = new PrismaClient()
+  .$extends(withAccelerate())
