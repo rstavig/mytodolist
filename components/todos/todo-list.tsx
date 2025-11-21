24ec781194abacf69edd6c37b7121d7650992556
clickalot <rstavig@comcast.net>
2025-11-21 11:48:20 -0600

    fix typing errors

--- components/todos/todo-list.tsx
+++ components/todos/todo-list.tsx
@@ -1,18 +1,28 @@
-const TodoList = ({data, title}: {data: any; title?: string}) => {
-    console.log(data.todos)
+
+// Define the shape of a single todo item
+interface Todo {
+  id: string | number;
+  todo: string;
+}
+
+// Define the props interface 
+interface TodoListProps {
+  title: string;
+  data: Todo[];
+}
+
+// Functional component to display a list of todos
+const TodoList = ({ title, data }: TodoListProps) => {
+console.log(data);
     return (
         <>
          <h2 className='h2-bold mb-4'>{title}</h2>
-        
-
-
          {data.length > 0 ? (
              <div className=''>
-               {data.map((item: any) => (
+               {data.map((item) => (
                 <ul key={item.id}>
                  <li>{item.id}</li>
                  <li>{item.todo}</li>
-
                  </ul>
                ))}
              </div>
